47af24e33931b8f3a351687b9c9c6327630dbada
michalrusak <michal1406@onet.pl>
2024-09-04 13:31:58 +0200

    fix text center

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -8,17 +8,17 @@ const Gallery = () => {
   return (
     <section className="min-h-[50vh] py-20 overflow-hidden">
       <div className="px-32">
-        <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-xl xl:text-4xl text-blue-700">
+        <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-xl xl:text-4xl text-blue-700 text-center md:text-left">
           Prezentacja firmy
         </h3>
-        <h2 className="text-2xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl">
+        <h2 className="text-2xl sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl text-center md:text-left">
           Zobacz naszą galerię zdjęć
         </h2>
-        <div className="flex justify-start items-center flex-wrap mt-10">
-          <button className="text-xl text-blue-700 py-2 mr-8 mb-4 px-1 border-b-2 border-current font-medium">
+        <div className="flex flex-center md:justify-start items-center flex-col md:flex-row mt-10">
+          <button className="text-xl text-blue-700 py-2 md:mr-8 mb-4 px-1 border-b-2 border-current font-medium">
             Samochody osobowe
           </button>
-          <button className="text-xl py-2 pr-8 px-1 rounded font-medium">
+          <button className="text-xl py-2 md:pr-8 mb-4 px-1 rounded font-medium">
             Samochody dostawcze
           </button>
         </div>
